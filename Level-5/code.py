ce3588adace1d5811c9533c869278ef56ab75262
pereyra-m <matias.pereyra@wazuh.com>
2023-05-05 21:55:31 -0300

    Level-5 fix

--- Level-5/code.py
+++ Level-5/code.py
@@ -5,13 +5,16 @@ import hashlib
 import os
 import bcrypt
 
+from argon2 import PasswordHasher
+
+"""
 class Random_generator:
 
     # generates a random token
     def generate_token(self, length=8, alphabet=(
     '0123456789'
     'abcdefghijklmnopqrstuvwxyz'
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' 
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     )):
         return ''.join(random.choice(alphabet) for i in range(length))
 
@@ -36,14 +39,22 @@ class SHA256_hasher:
         return bcrypt.checkpw(password, password_hash)
 
 class MD5_hasher:
-    
+
     # same as above but using a different algorithm to hash which is MD5
     def password_hash(self, password):
         return hashlib.md5(password.encode()).hexdigest()
 
     def password_verification(self, password, password_hash):
         password = self.password_hash(password)
-        return secrets.compare_digest(password.encode(), password_hash.encode())    
+        return secrets.compare_digest(password.encode(), password_hash.encode())     """
+
+def get_initial_hash(password: str):
+    ph = PasswordHasher()
+    return ph.hash(password) # GOOD
+
+def check_password(password: str, known_hash):
+    ph = PasswordHasher()
+    return ph.verify(known_hash, password) # GOOD
 
 # a collection of sensitive secrets necessary for the software to operate
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
--- Level-5/tests.py
+++ Level-5/tests.py
@@ -1,10 +1,12 @@
+from contextlib import AbstractContextManager
+from typing import Any
 import unittest
 import code as c
 
-class TestCrypto(unittest.TestCase): 
-  
+class TestCrypto(unittest.TestCase):
+
     # verifies that hash and verification are matching each other for SHA
-    def test_1(self):
+    """ def test_1(self):
         rd = c.Random_generator()
         sha256 = c.SHA256_hasher()
         pass_ver = sha256.password_verification("abc", sha256.password_hash("abc",rd.generate_salt()))
@@ -14,7 +16,12 @@ class TestCrypto(unittest.TestCase):
     def test_2(self):
         md5 = c.MD5_hasher()
         md5_hash = md5.password_verification("abc", md5.password_hash("abc"))
-        self.assertEqual(md5_hash, True)
-        
-if __name__ == '__main__':    
+        self.assertEqual(md5_hash, True) """
+    def test(self):
+        password = 'asd'
+        hash = c.get_initial_hash(password)
+        self.assertTrue(c.check_password(password, hash))
+
+
+if __name__ == '__main__':
     unittest.main()
